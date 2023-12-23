cadc8a9d299cd87c05a86cf2e0aec738b5d30686
ArtfulLee <aastepsb@yandex.ru>
2023-12-23 13:32:14 +0300

    feat: add lesson11

--- src/js/hw/lesson11.js
+++ src/js/hw/lesson11.js
@@ -1,15 +1,26 @@
 ////////////////////////////////////////////////////
 // Выведите в цикле простые числа от 2 до 100. Простое число - это число, которое делится только на себя и на 1. Пример простых чисел.
 ////////////////////////////////////////////////////
+
 function simpleNumbers() {
-  const simpleNumberArray = [] // Массив для простых чисел
+  // Создаем массив для простых чисел
+  const simpleNumberArray = []
 
   for (let index = 2; index <= 100; index++) {
-    let countNumbersDivisors = index // счетчик делителей, для нахождения делителя без остатка отличного от числа по index или 1
-    let simpleNumberByIndex = true // Переменная, для определения, является ли чесло простым
+    // Исключаем четные числа, кроме index === 2
+    if (index % 2 === 0 && index !== 2) continue
 
-    for (; countNumbersDivisors > 0; countNumbersDivisors--) {
-      if (countNumbersDivisors === 1 || countNumbersDivisors === index) continue
+    // Создаем счетчик делителей, для нахождения делителя без остатка отличного от числа по index или 1
+    let countNumbersDivisors = index
+
+    // Переменная, для определения, является ли число простым
+    let simpleNumberByIndex = true
+
+    // "countNumbersDivisors > 1" - исключаем проверку деления index на 1
+    for (; countNumbersDivisors > 1; countNumbersDivisors--) {
+      // исключаем проверку деления index на само себя и исключаем четные делители
+      if (countNumbersDivisors === index || countNumbersDivisors % 2 === 0) continue
+
       if (index % countNumbersDivisors === 0) {
         simpleNumberByIndex = false
         break
