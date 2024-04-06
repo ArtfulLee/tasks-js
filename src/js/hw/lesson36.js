240e520e1f608e4646e9937cb1511614130f3554
ArtfulLee <aastepsb@yandex.ru>
2024-04-06 10:52:38 +0300

    lesson36 WIP

---
+++ src/js/hw/lesson36.js
@@ -0,0 +1,52 @@
+/* ***********************************************************
+Завершите функцию, чтобы она суммировала все оценки за курс learnEnglish в массиве.
+*********************************************************** */
+const students = [
+  {
+    name: 'Sam Doe',
+    age: 24,
+    learnEnglish: {
+      startYear: 2020,
+      grade: 18,
+    },
+  },
+  {
+    name: 'Charlie Bron',
+    age: 31,
+    learnEnglish: {
+      startYear: 2021,
+      grade: 19,
+    },
+  },
+]
+
+function getSumGrades(students) {
+  // your code
+}
+
+// Sample usage - do not modify
+console.log(getSumGrades(students)) // 37
+
+/* ***********************************************************
+Завершите функцию, чтобы она возвращала массив содержащий полное имя каждого пользователя.
+*********************************************************** */
+
+const users = [
+  {
+    firstName: 'Sam',
+    lastName: 'Blue',
+    age: 21,
+  },
+  {
+    firstName: 'Charlie',
+    lastName: 'Bon',
+    age: 38,
+  },
+]
+
+function getFullNames(users) {
+  // your cude
+}
+
+// Sample usage - do not modify
+console.log(getFullNames(users)) // [Sam Blue, Charlie Bon]
